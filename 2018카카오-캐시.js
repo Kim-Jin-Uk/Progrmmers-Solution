689abf60ea139694b189c7d07c2c326eaa5cfbff
김진욱 <76549646+Kim-Jin-Uk@users.noreply.github.com>
2022-09-19 20:17:38 +0900

    Refactoring / 성능향상 0.86ms -> 0.57ms, 가독성 개선

--- 2018카카오-캐시.js
+++ 2018카카오-캐시.js
@@ -1,45 +1,25 @@
 function solution(cacheSize, cities) {
-    let answer = 0
-    // 캐시를 담을 큐 생성
-    let cache = []
-    for(const c of cities){
-        // 대소문자 구분을 안함
-        const city = c.toLowerCase()
-        // 캐시에 여유가 있을때
-        if(cache.length < cacheSize){
-            // 포함일때
-            if(cache.includes(city)){
-                answer ++
-                // 우선순위 조정
-                cache = cache.filter(v=>v!==city)
-                if(cache.length < cacheSize) cache.push(city)
-            }
-            // 미포함일때
-            else{
-                answer += 5
-                cache.push(city) 
-            }
+    // 캐시 공간 없을시 바로리턴
+    if (cacheSize === 0) return 5 * cities.length
+    let answer = 0, cache = []
+    cities.forEach(city => {
+        // 대소구분 X
+        city = city.toLowerCase()
+        let idx = cache.indexOf(city)
+        // 캐시 존재 O
+        if (idx > -1) {
+            cache.splice(idx, 1)
+            answer += 1
+        } 
+        // 캐시 존재 X
+        else {
+            // 제거할 캐시 제거
+            if (cache.length >= cacheSize) cache.shift()
+            answer += 5
         }
-        // 캐시가 꽉 찻을때
-        else{
-            // 포함일때
-            if(cache.includes(city)){
-                answer ++
-                // 우선순위 조정
-                cache = cache.filter(v=>v!==city)
-                // 캐시 최대길이 0일때 처리
-                if(cache.length < cacheSize) cache.push(city)
-            }
-            // 미포함일때
-            else{
-                answer += 5
-                // 큐에서 빼내고 새로 추가
-                cache.shift()
-                // 캐시 최대길이 0일때 처리
-                if(cache.length < cacheSize)cache.push(city)
-            }
-        }
-    }
+        // 캐시에 넣어주기
+        cache.push(city)
+    })
     return answer
 }
 
