438420a083ff35870dbb221ce789dc1148bd1347
김진욱 <76549646+Kim-Jin-Uk@users.noreply.github.com>
2022-11-26 19:19:00 +0900

    Update/ level3/ 카운트다운 개선

--- 카운트다운.js
+++ 카운트다운.js
@@ -1,29 +1,21 @@
 function solution(target) {
-  const dp = new Array(300000).fill(null).map((_) => [Infinity, 0]);
-  const targetList = new Array(20).fill(null).map((_, idx) => idx + 1);
+  const dp = new Array(target + 1).fill(0).map((_) => [Infinity, 0]);
+  const targetList = new Array(20).fill(0).map((_, idx) => idx + 1);
   dp[0][0] = 0;
   for (let i = 0; i < target; i++) {
     for (let j of targetList) {
-      if (dp[i + j * 1][0] >= dp[i][0] + 1) {
-        if (dp[i + j * 1][0] === dp[i][0] + 1)
-          dp[i + j * 1][1] = Math.max(dp[i + j * 1][1], dp[i][1] + 1);
-        else dp[i + j * 1] = [dp[i][0] + 1, dp[i][1] + 1];
-      }
-      if (dp[i + j * 2][0] >= dp[i][0] + 1) {
-        if (dp[i + j * 2][0] === dp[i][0] + 1)
-          dp[i + j * 2][1] = Math.max(dp[i + j * 2][1], dp[i][1] + 0);
-        else dp[i + j * 2] = [dp[i][0] + 1, dp[i][1] + 0];
-      }
-      if (dp[i + j * 3][0] >= dp[i][0] + 1) {
-        if (dp[i + j * 3][0] === dp[i][0] + 1)
-          dp[i + j * 3][1] = Math.max(dp[i + j * 3][1], dp[i][1] + 0);
-        else dp[i + j * 3] = [dp[i][0] + 1, dp[i][1] + 0];
-      }
+      setNext(i, j, 1);
+      setNext(i, j * 2, 0);
+      setNext(i, j * 3, 0);
+      setNext(i, 50, 1);
     }
-    if (dp[i + 50][0] >= dp[i][0] + 1) {
-      if (dp[i + 50][0] === dp[i][0] + 1)
-        dp[i + 50][1] = Math.max(dp[i + 50][1], dp[i][1] + 1);
-      dp[i + 50] = [dp[i][0] + 1, dp[i][1] + 1];
+  }
+  function setNext(i, score, add) {
+    if (i + score > target) return;
+    if (dp[i + score][0] >= dp[i][0] + 1) {
+      if (dp[i + score][0] === dp[i][0] + 1)
+        dp[i + score][1] = Math.max(dp[i + score][1], dp[i][1] + add);
+      else dp[i + score] = [dp[i][0] + 1, dp[i][1] + add];
     }
   }
   return dp[target];
