60057e9adb125d2e494367dd7a41ab0b878b7e23
김진욱 <76549646+Kim-Jin-Uk@users.noreply.github.com>
2022-12-10 12:26:22 +0900

    Refactoring

--- 2019카카오-튜플.js
+++ 2019카카오-튜플.js
@@ -1,43 +1,51 @@
 function solution(s) {
-    const setS = []
-    let child = []
-    let char2Num = ''
-    // 문자열을 2차원 배열로 전환
-    for(const char of s){
-        // 열린부분은 스킵
-        if(char === '{') continue
-        // 닫힌부분은 맨 마지막이 아니라면 부분집합을 집합에 넣어주기
-        else if(char === '}'){
-            if(char2Num !== ''){
-                child.push(Number(char2Num))
-                char2Num = ''
-                setS.push([...child])
-                child = []
-            }
-        }
-        // 구분자의 경우 부분집합의 구분자가 아니라면 부분집합에 원소 넣어주기
-        else if(char === ','){
-            if(char2Num !== ''){
-                child.push(Number(char2Num))
-                char2Num = ''
-            }
-        }
-        // 그외의 경우는 숫자로 바꾸기 전 문자열에 넣어주기
-        else{
-            char2Num += char
-        }
+  const setS = [];
+  let child = [];
+  let char2Num = "";
+  // 문자열을 2차원 배열로 전환
+  for (const char of s) {
+    // 열린부분은 스킵
+    if (char === "{") continue;
+    // 닫힌부분은 맨 마지막이 아니라면 부분집합을 집합에 넣어주기
+    else if (char === "}") {
+      if (char2Num !== "") {
+        child.push(Number(char2Num));
+        char2Num = "";
+        setS.push([...child]);
+        child = [];
+      }
     }
-    // 길이순으로 정렬
-    setS.sort((a,b)=>a.length-b.length)
-    const prevs = []
-    for(const child of setS){
-        for(const num of child){
-            // 튜플에 존재하지 않으면 새로운 원소이므로 넣오주고 break
-            if(!prevs.includes(num)){
-                prevs.push(num)
-                break
-            }
-        }
+    // 구분자의 경우 부분집합의 구분자가 아니라면 부분집합에 원소 넣어주기
+    else if (char === ",") {
+      if (char2Num !== "") {
+        child.push(Number(char2Num));
+        char2Num = "";
+      }
     }
-    return prevs
+    // 그외의 경우는 숫자로 바꾸기 전 문자열에 넣어주기
+    else {
+      char2Num += char;
+    }
+  }
+  // 길이순으로 정렬
+  setS.sort((a, b) => a.length - b.length);
+  const prevs = [];
+  for (const child of setS) {
+    for (const num of child) {
+      // 튜플에 존재하지 않으면 새로운 원소이므로 넣오주고 break
+      if (!prevs.includes(num)) {
+        prevs.push(num);
+        break;
+      }
+    }
+  }
+  return prevs;
+}
+function solution(s) {
+  return JSON.parse(s.replaceAll("{", "[").replaceAll("}", "]"))
+    .sort((a, b) => a.length - b.length)
+    .reduce((arr, v, n) => {
+      if (n) return arr.concat(v.filter((f) => !arr.includes(f)));
+      return v;
+    }, []);
 }
--- 2019카카오-후보키.js
+++ 2019카카오-후보키.js
@@ -1,36 +1,74 @@
 function solution(relation) {
-    const PKs = {}
-    // 유일성 검증
-    function uniquenessDFS(idx,PKObj){
-        if(idx > relation[0].length) return
-        const isUnique = Object.keys(PKObj).length ? new Set(relation.map((a)=>a.filter((b,i)=>PKObj[i]).join())).size === relation.length : false
-        if(isUnique) PKs[Object.keys(PKObj).sort((a,b)=>a-b).join()] = true
-        uniquenessDFS(idx+1,PKObj)
-        const copy = {...PKObj}
-        copy[idx] = true
-        uniquenessDFS(idx+1,copy)
+  const PKs = {};
+  // 유일성 검증
+  function uniquenessDFS(idx, PKObj) {
+    if (idx > relation[0].length) return;
+    const isUnique = Object.keys(PKObj).length
+      ? new Set(relation.map((a) => a.filter((b, i) => PKObj[i]).join()))
+          .size === relation.length
+      : false;
+    if (isUnique)
+      PKs[
+        Object.keys(PKObj)
+          .sort((a, b) => a - b)
+          .join()
+      ] = true;
+    uniquenessDFS(idx + 1, PKObj);
+    const copy = { ...PKObj };
+    copy[idx] = true;
+    uniquenessDFS(idx + 1, copy);
+  }
+  uniquenessDFS(0, {});
+  // 최소성 검증
+  while (true) {
+    const keys = Object.keys(PKs).sort((a, b) => a.length - b.length);
+    let isMinimality = true;
+    keys.forEach((key, idx) => {
+      const standard = new Array(relation[0].length).fill(false);
+      key.split(",").forEach((idx) => {
+        standard[idx] = true;
+      });
+      for (let i = idx + 1; i < keys.length; i++) {
+        const match = new Array(relation[0].length).fill(false);
+        keys[i].split(",").forEach((idx) => {
+          match[idx] = true;
+        });
+        let check = true;
+        standard.forEach((v, i) => {
+          if (v && !match[i]) check = false;
+        });
+        if (check) {
+          delete PKs[keys[i]];
+          isMinimality = false;
+        }
+      }
+    });
+    if (isMinimality) return keys.length;
+  }
+}
+
+function solution(relation) {
+  const cols = relation[0].length;
+  const rows = relation.length;
+  const sets = 1 << cols;
+  const answer = new Set();
+  for (let i = 1; i < sets; i++) {
+    const tmp = new Set();
+    for (let row = 0; row < rows; row++) {
+      let key = "";
+      for (let col = 0; col < cols; col++) {
+        if (i & (1 << col)) key = key + relation[row][col];
+      }
+      tmp.add(key);
     }
-    uniquenessDFS(0,{})
-    // 최소성 검증
-    while(true){
-        const keys = Object.keys(PKs).sort((a,b)=>a.length-b.length)
-        let isMinimality = true
-        keys.forEach((key,idx)=>{
-            const standard = new Array(relation[0].length).fill(false)
-            key.split(',').forEach((idx)=>{standard[idx] = true})
-            for(let i = idx+1; i < keys.length; i++){
-                const match = new Array(relation[0].length).fill(false)
-                keys[i].split(',').forEach((idx)=>{match[idx] = true})
-                let check = true
-                standard.forEach((v,i)=>{
-                    if(v && !match[i]) check = false
-                })
-                if(check){
-                    delete PKs[keys[i]]
-                    isMinimality = false
-                }
-            }
-        })
-        if(isMinimality) return keys.length
+    if (tmp.size === rows) answer.add(i);
+  }
+  for (let i of answer) {
+    for (let j of answer) {
+      if (i >= j) continue;
+      if ((i & j) === i) answer.delete(j);
     }
+  }
+
+  return answer.size;
 }
